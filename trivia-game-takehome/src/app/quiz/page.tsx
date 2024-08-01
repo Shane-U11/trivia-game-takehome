b295c5432770c2e3ab4d8fb1601afce2713465d4
Shane-U11 <shaneulrich@icloud.com>
2024-08-01 12:31:54 -0700

    created shuffleArray func to randomize answers and set those to state as well as the questions to the answers

--- trivia-game-takehome/src/app/quiz/page.tsx
+++ trivia-game-takehome/src/app/quiz/page.tsx
@@ -2,6 +2,16 @@
 import React, { useEffect, useState } from 'react'
 
 const Quiz = () => {
+    const [questionsAndAnswers, setQuestionsAndAnswers] = useState<{ question: string, correct_answer: string, answers: string[] }[]>([])
+
+    const shuffleArray = (array: any[]) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
      useEffect(() => {
         const fetchProblems = async () => {
             try {
@@ -10,10 +20,17 @@ const Quiz = () => {
                 const responseResults = response.results.map((data: any) => {
                     const correctAnswer = data.correct_answer
                     const incorrectAnswers = data.incorrect_answers
-                    console.log('C', correctAnswer)
-                    console.log('I', incorrectAnswers)
+                    // console.log('C', correctAnswer)
+                    // console.log('I', incorrectAnswers)
+                    const answersArr = shuffleArray([correctAnswer, ...incorrectAnswers])
+                    // console.log('HERE', answersArr, correctAnswer)
+                    return {
+                        question: data.question,
+                        correct_answer: correctAnswer,
+                        allAnswers: answersArr
+                    }
                 })
-                console.log(responseResults)
+                setQuestionsAndAnswers(responseResults)
             } catch (error) {
                 console.error('Error fetching data', error)
             }
